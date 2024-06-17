97525fa17ef297df92b8b3c4efa1c3e5913185da
cdulicki <cdulicki@gmail.com>
2024-06-16 21:52:49 -0400

    Create header.js

---
+++ scripts/header.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const headerHTML = `
+        <header>
+            <img src="images/logo.png" alt="Lifters Link Logo" class="logo">
+            <nav>
+                <ul>
+                    <li><a href="get_notified.html">Get Notified</a></li>
+                    <li><a href="tune_in.html">Tune In</a></li>
+                    <li><a href="about_us.html">About Us</a></li>
+                </ul>
+            </nav>
+        </header>
+    `;
+    document.body.insertAdjacentHTML("afterbegin", headerHTML);
+});
