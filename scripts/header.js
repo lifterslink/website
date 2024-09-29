2a6616aad0d78e881958c8437a5d7570ff8cee25
cdulicki <cdulicki@gmail.com>
2024-09-29 17:29:12 -0400

    Update header.js
    Deleted about us from header

--- scripts/header.js
+++ scripts/header.js
@@ -2,11 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const headerHTML = `
         <header>
             <img src="logo.png" alt="Lifters Link Logo" class="logo">
-            <nav>
-                <ul>
-                    <li><a href="about_us.html">About Us</a></li>
-                </ul>
-            </nav>
         </header>
     `;
     document.body.insertAdjacentHTML("afterbegin", headerHTML);
